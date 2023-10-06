9b890787853be03aff795066dcb2b0e04e7ad4fb
FRANGUILLEN\Usuario <fjavierguillenpuente@gmail.com>
2023-10-07 01:33:26 +0200

    no message

--- ejerciciosJs/tema2.3/ejercicio17.js
+++ ejerciciosJs/tema2.3/ejercicio17.js
@@ -8,10 +8,39 @@ function detener() {
   clearInterval(tempo);
 }
 
-let segundoMen = setTimeout(para, 3000);
+function hanPasado3Segundos() {
+   console.log("Han pasado 3 segundos");
+ }
+ 
+ function para() {
+  setTimeout(hanPasado3Segundos, 3000);
+ }
 
-function segundoMen() {}
-
-function para() {
-  console.log("Han pasado 3 segundos");
+function fecha(){
+var fecha= new Date().toLocaleTimeString();
 }
+ 
+
+
+ function actualizarReloj() {
+   var tempo2 = setInterval(fecha,1000);
+ }
+ 
+
+function pararReloj() {
+  clearInterval(tempo2, 5000);
+}
+
+
+function nuevaVentana() {
+  var nuevaVentana = window.open("","nuevaVentana","toolbar=no,location=no,menubar=no,width=300,height=300,resizable= yes,scrollbars= yes,top= 500,left = 500");
+  nuevaVentana.document.write("<p>Hora actual</p>");
+  nuevaVentana.document.write(actualizarReloj());
+  cerrarVentana();
+}
+
+function cerrarVentana(){
+  document.window.nuevaVentana.close(10000);
+}
+
+
