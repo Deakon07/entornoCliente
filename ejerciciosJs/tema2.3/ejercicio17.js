1761341c9ce8413dd774750f1e4f73f03c9a3c52
CEP-INFOR4-PC09\TECH <fguillen1749@ceuandalucia.es>
2023-10-09 18:55:51 +0200

    no message

--- ejerciciosJs/tema2.3/ejercicio17.js
+++ ejerciciosJs/tema2.3/ejercicio17.js
@@ -9,38 +9,30 @@ function detener() {
 }
 
 function hanPasado3Segundos() {
-   console.log("Han pasado 3 segundos");
- }
- 
- function para() {
+  console.log("Han pasado 3 segundos");
+}
+
+function para() {
   setTimeout(hanPasado3Segundos, 3000);
- }
-
-function fecha(){
-var fecha= new Date().toLocaleTimeString();
 }
- 
-
-
- function actualizarReloj() {
-   var tempo2 = setInterval(fecha,1000);
- }
- 
-
-function pararReloj() {
-  clearInterval(tempo2, 5000);
-}
-
 
 function nuevaVentana() {
-  var nuevaVentana = window.open("","nuevaVentana","toolbar=no,location=no,menubar=no,width=300,height=300,resizable= yes,scrollbars= yes,top= 500,left = 500");
+  var nuevaVentana = window.open(
+    "",
+    "nuevaVentana",
+    "toolbar=no,location=no,menubar=no,width=300,height=300,resizable= yes,scrollbars= yes,top= 500,left = 500"
+  );
+
   nuevaVentana.document.write("<p>Hora actual</p>");
-  nuevaVentana.document.write(actualizarReloj());
-  cerrarVentana();
+  function actualizarReloj() {
+    var fecha = new Date();
+    nuevaVentana.document.write(fecha.toLocaleTimeString());
+    var update = setInterval(actualizarReloj, 1000);
+
+    setTimeout(actualizarReloj, 5000);
+    cerrarVentana();
+  }
+  function cerrarVentana() {
+    setInterval(nuevaVentana.close, 10000);
+  }
 }
-
-function cerrarVentana(){
-  document.window.nuevaVentana.close(10000);
-}
-
-
