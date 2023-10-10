b593e14cae2c5d812d30f31d46cceb7d89f6f3c1
CEP-INFOR4-PC09\TECH <fguillen1749@ceuandalucia.es>
2023-10-10 20:13:00 +0200

    no message

--- ejerciciosJs/tema2.3/ejercicio17.js
+++ ejerciciosJs/tema2.3/ejercicio17.js
@@ -16,23 +16,29 @@ function para() {
   setTimeout(hanPasado3Segundos, 3000);
 }
 
-function nuevaVentana() {
-  var nuevaVentana = window.open(
+var nuevaVentana;
+function abrirVentana() {
+  nuevaVentana = window.open(
     "",
     "nuevaVentana",
     "toolbar=no,location=no,menubar=no,width=300,height=300,resizable= yes,scrollbars= yes,top= 500,left = 500"
   );
-
   nuevaVentana.document.write("<p>Hora actual</p>");
-  function actualizarReloj() {
-    var fecha = new Date();
-    nuevaVentana.document.write(fecha.toLocaleTimeString());
-    var update = setInterval(actualizarReloj, 1000);
+  setTimeout(cerrarInterval, 5000);
+  setTimeout(cerrarVentana, 10000);
+}
 
-    setTimeout(actualizarReloj, 5000);
-    cerrarVentana();
-  }
-  function cerrarVentana() {
-    setInterval(nuevaVentana.close, 10000);
-  }
+function actualizarReloj() {
+  var fecha = new Date();
+  nuevaVentana.document.write(fecha.toLocaleTimeString() + "<br>");
+}
+
+var update = setInterval(actualizarReloj, 1000);
+
+function cerrarInterval() {
+  clearInterval(update);
+}
+
+function cerrarVentana() {
+  nuevaVentana.close();
 }
