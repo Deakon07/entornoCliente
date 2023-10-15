214908abce790bf573c2a9ac1d0856b4ca3f8fb8
FRANGUILLEN\Usuario <fjavierguillenpuente@gmail.com>
2023-10-15 13:55:00 +0200

    Ejercicio 22 terminado!

--- ejerciciosJs/tema2.3/Ejercicio 22/datos.js
+++ ejerciciosJs/tema2.3/Ejercicio 22/datos.js
@@ -3,8 +3,18 @@ function abrirPagina() {
 }
 
 function datosPadre() {
-  window.open("window.html", "ventanaPadre");
-  document.write(window.document.write(escribirEnPadre.nombre.value));
-  document.write(window.document.write(escribirEnPadre.apellidos.value));
-  document.write(window.document.write(escribirEnPadre.FechaNacimiento.value));
+  window.open("window.html", "padre");
+  window.opener.document.write(escribirEnPadre.nombre.value + " " + escribirEnPadre.apellidos.value +"<br>");
+  var fechaUsuario = new Date(escribirEnPadre.FechaNacimiento.value);
+  window.opener.document.write(`${fechaUsuario.getDate()} / ${fechaUsuario.getMonth()+1} / ${fechaUsuario.getFullYear()} <br>`);
+  var fechaActual = new Date();
+  var anoActual= fechaActual.getFullYear();
+  
+  window.opener.document.write("Los años bisiestos  entre tu fecha de nacimiento y el año actual son:<br>");
+
+  for (let i = fechaUsuario.getFullYear(); i <= anoActual; i++) {
+    if ((i % 4 === 0 && i % 100 !== 0) || i % 400 === 0) {
+    window.opener.document.write(i + "<br>");
+  }
+}
 }
