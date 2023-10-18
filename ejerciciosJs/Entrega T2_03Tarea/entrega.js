9c6916af0cdb4f730f14e8ef25b8cbb3b653ef47
FRANGUILLEN\Usuario <fjavierguillenpuente@gmail.com>
2023-10-18 13:33:11 +0200

    primer botón del ejercicio de la entrega

--- ejerciciosJs/Entrega T2_03Tarea/entrega.js
+++ ejerciciosJs/Entrega T2_03Tarea/entrega.js
@@ -2,11 +2,26 @@
 const pantallaAlto = window.innerHeight;
 const pantallaAncho = window.innerWidth;
 
+// Averiguamos la posición inicial (X, Y) de los coches obteniendo las imágenes por su ID.
+var cocheRojo = document.getElementById('cocheRojo');
+var RojoPosY = cocheRojo.offsetTop;  // Posición vertical Y
+var RojoPosX = cocheRojo.offsetLeft; // Posición horizontal X
 
-// Obtenemos las imágenes de los coches por su ID
-const coche1 = document.getElementById('cocheRojo');
-const coche2 = document.getElementById('cocheAzul');
+var cocheAzul = document.getElementById('cocheAzul');
+var AzulPosY = cocheAzul.offsetTop;  // Posición vertical Y
+var AzulPosX = cocheAzul.offsetLeft; // Posición horizontal X
 
-//Le ponemos la ruta relativa de cada coche
-coche1.src = '/images/coche rojo.png';
-coche2.src = '/images/coche azul.png';
+var primerBoton= setInterval(comenzar,1000);
+
+function comenzar() {
+    if (RojoPosX < pantallaAncho && AzulPosX < pantallaAncho) {
+            RojoPosX= RojoPosX + 1;  // Mueve el coche rojo hacia la derecha
+            AzulPosX= AzulPosX + 2;  // Mueve el coche azul hacia la derecha
+    } else {
+            // Los coches han llegado al final, detenemos el intervalo
+            clearInterval(primerBoton);
+        }
+        
+    }
+
+   
